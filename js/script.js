8286a40a7565f8d3ef5e698b687ccd7f6e74259e
Salvador Torres Rodriguez <a00825038@itesm.mx>
2023-01-22 23:11:07 -0600

    Se completo TodoClear y TodoMark

--- js/script.js
+++ js/script.js
@@ -3,19 +3,30 @@ function TodoPost(e){ // La 'e' contiene toda la info del evento que se esta dis
     
     e.preventDefault()
     let toDo = $("#todoText").val()
-    $("#todoList").append(` <input type="checkbox" /> <label> ${toDo} </label> <br> `)
+    $("#todoList").append(` <input type="checkbox" name="todo" /> <label> ${toDo} </label> <br> `)
 }
 
+// En estas funciones de abajo, no es necesario agregar el evento de e.preventDefault()
+// porque los 3 botones de la derecha estan fuera del formulario.
+
+// Remover la seleccion de checkbox de todos los elementos de la lista
 function TodoClear(){
-    
+    let todos = $("[name='todo']") // De esta manera selecciono todas las checkboxes
+    for(const element of todos){
+     element.checked = false
+    }
 }
 
+// Marca todos los checkboxes de la lista
 function TodoMark(){
-    
+    let todos = $("[name='todo']") // De esta manera selecciono todas las checkboxes
+    for(const element of todos){
+     element.checked = true
+    }
 }
 
 function TodoDelete(){
-    
+    console.log("aaaa")
 }
 
 $("#ButtonPost").click(TodoPost)
